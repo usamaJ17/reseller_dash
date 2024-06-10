878ce58ae01716ee50f538e3b2737f2e7e48ebed
usamaJ17 <usamajalal17@gmail.com>
2024-06-10 12:48:40 +0500

    upd

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ use App\Http\Controllers\Api\AuthController;
 // });
 
 Route::post('register',[AuthController::class,'register']);
-Route::post('login',[AuthController::class,'login']);
+Route::post('login',[AuthController::class,'login'])->middleware('cros');;
 Route::post('logout',[AuthController::class,'logout'])
   ->middleware('auth:sanctum');
