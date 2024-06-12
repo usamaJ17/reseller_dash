d901f67a8731007ffc9637d01696ff801ab6cf3b
usamaJ17 <usamajalal17@gmail.com>
2024-06-12 18:32:56 +0500

    git ignore

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login'])->middleware('cros');;
+Route::post('logout',[AuthController::class,'logout'])
+  ->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum','add.token'])->group(function () {
+  Route::get('products',[ProductController::class,'getAll']);
+  Route::get('product_detail/{id}',[ProductController::class,'getDetails']);
+  Route::get('product_by_category/{id}',[ProductController::class,'getProductByCategory']);
+  Route::get('category',[ProductController::class,'getAllCategory']);
+  Route::resource('client', ClientController::class);
+});
