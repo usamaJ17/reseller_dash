5dffa5c8a53a1727b14fdc0865e72630fa8132e6
usamaJ17 <usamajalal17@gmail.com>
2024-06-16 18:40:33 +0500

    up

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,7 +30,7 @@ class ProductController extends Controller
         $response = Http::withToken(Auth::user()->jwt_token)
         ->get(env('ADMIN_PORTAL_URL').'/product-details'.'/'.$id);   
         $responseJson = $response->json(); 
-        dd($responseJson);
+        dd($responseJson['data']['price']);
         return response($responseJson);
     }
 }
