7324d464121c73eaa62d3a02505f041ddb119bb5
usamaJ17 <usamajalal17@gmail.com>
2024-06-27 00:08:20 +0500

    upd

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -193,7 +193,6 @@ class AuthController extends Controller
     public function changeResellerStatus(Request $request){
         $user = User::where('email',$request->email)->first();
         $user->is_verified = $request->status;
-        $user->tokens()->delete();
         $user->save();
         return response()->json([
             'status'  => 202,
