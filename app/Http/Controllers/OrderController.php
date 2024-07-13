d72b3a27e8674c0094969b5cf1e295e9ebacbd86
usamaJ17 <usamajalal17@gmail.com>
2024-07-13 16:31:25 +0500

    ads

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -69,7 +69,6 @@ class OrderController extends Controller
             $quantity[] = $temp_data;
             $valid_key++;
         }
-        dd($cart_errors);
         if(!empty($cart_errors)){
             $requestParameters = [
                 'user_id' => Auth::user()->portal_id,
