d7bebec3f1e65ef1028d086321d31a353779aa1f
usamaJ17 <usamajalal17@gmail.com>
2024-07-13 12:36:17 +0500

    upd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -54,7 +54,6 @@ class OrderController extends Controller
             }
             $trx_id = $response_1->json()['carts'][0]['trx_id'];
             $price = $price + ($response_1->json()['carts'][0]['quantity'] * $response_1->json()['carts'][0]['price']);
-            dd();
             if($valid_key >= sizeof($response_1->json()['carts'])){
                 $valid_key--;
                 $temp_data = [
